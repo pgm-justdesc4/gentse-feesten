8ab120a9a1ad396d5f9e85e4c4a5fce86ded9e04
pgm-justdesc4 <justin.descan@student.arteveldehs.be>
2023-12-18 15:27:18 +0100

    placed reload-logo in IIFE

--- static/js/reload-logo.js
+++ static/js/reload-logo.js
@@ -1,31 +1,33 @@
-const $logo = document.getElementById("logo");
-const $letter = document.getElementById("logo-letter");
-const $logoFooter = document.getElementById("logo-footer");
-const $letterFooter = document.getElementById("logo-letter-footer");
+(function () {
+  const $logo = document.getElementById("logo");
+  const $letter = document.getElementById("logo-letter");
+  const $logoFooter = document.getElementById("logo-footer");
+  const $letterFooter = document.getElementById("logo-letter-footer");
 
-function getRandomNumber() {
-  return Math.floor(Math.random() * 6 + 1);
-}
+  function getRandomNumber() {
+    return Math.floor(Math.random() * 6 + 1);
+  }
 
-const number = getRandomNumber();
+  const number = getRandomNumber();
 
-function getHTMLForLogos() {
-  return `<img src="static/img/Gentse Feesten Logos/GF-logo-2023-${number}.svg" alt="Gente Feesten logo">`;
-}
+  function getHTMLForLogos() {
+    return `<img src="static/img/Gentse Feesten Logos/GF-logo-2023-${number}.svg" alt="Gente Feesten logo">`;
+  }
 
-function getHTMLForLetters() {
-  return `<img src="static/img/Gentse Feesten Logos/campagne-${number}.png" alt="Gente Feesten logo">`;
-}
+  function getHTMLForLetters() {
+    return `<img src="static/img/Gentse Feesten Logos/campagne-${number}.png" alt="Gente Feesten logo">`;
+  }
 
-function buildUI() {
-  $logo.innerHTML = getHTMLForLogos();
-  $letter.innerHTML = getHTMLForLetters();
-  $logoFooter.innerHTML = getHTMLForLogos();
-  $letterFooter.innerHTML = getHTMLForLetters();
-}
+  function buildUI() {
+    $logo.innerHTML = getHTMLForLogos();
+    $letter.innerHTML = getHTMLForLetters();
+    $logoFooter.innerHTML = getHTMLForLogos();
+    $letterFooter.innerHTML = getHTMLForLetters();
+  }
 
-function initialize() {
-  buildUI();
-}
+  function initialize() {
+    buildUI();
+  }
 
-initialize();
+  initialize();
+})();
