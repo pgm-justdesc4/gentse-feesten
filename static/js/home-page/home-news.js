4f1248c70f55b435eb568df30b769e0e0b019720
pgm-justdesc4 <justin.descan@student.arteveldehs.be>
2023-12-31 17:01:06 +0100

    made better file structure for js

--- static/js/home-page/home-news.js
+++ static/js/home-page/home-news.js
@@ -10,13 +10,15 @@
     news.forEach((newsPost) => {
       html += `
       <article>
-        <h3>${newsPost.title}</h3>
-        <div class="arrow-right">
-            <div class="arrow-helper"></div>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-                <path fill="currentColor"
-                d="M12.5 7c.384 0 .768.146 1.06.439l8.26 8.261-8.26 8.261a1.5 1.5 0 0 1-2.121-2.122l6.14-6.14-6.14-6.139a1.502 1.502 0 0 1 1.06-2.561z" /></svg>
+        <a href="events/detail.html?id:${newsPost.id}">
+            <p>${newsPost.title}</p>
+            <div class="arrow-right">
+                <div class="arrow-helper"></div>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                    <path fill="currentColor"
+                    d="M12.5 7c.384 0 .768.146 1.06.439l8.26 8.261-8.26 8.261a1.5 1.5 0 0 1-2.121-2.122l6.14-6.14-6.14-6.139a1.502 1.502 0 0 1 1.06-2.561z" /></svg>
             </div>
+        </a>
       </article>`;
     });
     return html;
